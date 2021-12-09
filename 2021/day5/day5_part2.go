9517923287d3acae0b8e74f669a54ddc445276b7
Jan Herber <jaydee@DESKTOP-GTJ767I.localdomain>
2021-12-09 16:45:59 +0100

    Add day 5

---
+++ 2021/day5/day5_part2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	input, _ := os.Open("input.txt")
+	defer input.Close()
+	sc := bufio.NewScanner(input)
+	lines_in_point := make(map[string]int)
+
+	for sc.Scan() {
+		var start, end [2]int
+		fmt.Sscanf(sc.Text(), "%d,%d -> %d,%d", &(start[0]), &(start[1]), &(end[0]), &(end[1]))
+
+		lines_in_point[fmt.Sprintf("%d,%d", start[0], start[1])]++
+		for start[0] != end[0] || start[1] != end[1] {
+			if start[0] > end[0] {
+				start[0]--
+			} else if start[0] < end[0] {
+				start[0]++
+			}
+			if start[1] > end[1] {
+				start[1]--
+			} else if start[1] < end[1] {
+				start[1]++
+			}
+			lines_in_point[fmt.Sprintf("%d,%d", start[0], start[1])]++
+		}
+	}
+
+	var count_points int
+	for _, v := range lines_in_point {
+		if v > 1 {
+			count_points++
+		}
+	}
+	fmt.Println(count_points)
+}
