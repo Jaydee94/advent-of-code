ad8ae9d80e542033707fbb0dafa9a8997f673c2c
jaydee <jaydee@Huawei-Book.fritz.box>
2022-12-05 21:15:21 +0100

    Add 2022 day5

---
+++ 2022/day5/day5_part1.py
@@ -0,0 +1,32 @@
+
+def main():
+    with open("input.txt", "r") as file:
+        input = file.read()
+
+    stack, commands = input.split("\n\n")
+    stacks = {}
+
+    for line in stack.split("\n")[-2::-1]:
+        for idx, element in enumerate(line[1:-1:4]):
+            if element == " ":
+                continue
+            if idx + 1 not in stacks:
+                stacks[idx + 1] = []
+            stacks[idx + 1].append(element)
+
+
+    stacks = stacks.copy()
+
+    for line in commands.split("\n"):
+        _, amount, _, source, _, destination = line.strip().split(" ")
+        amount, source, destination = int(amount), int(source), int(destination)
+        while amount > 0:
+            curr = stacks[source].pop()
+            stacks[destination].append(curr)
+            amount -= 1
+
+    print("".join([x[-1] for x in stacks.values()]))
+
+
+if __name__ == '__main__':
+    main()
