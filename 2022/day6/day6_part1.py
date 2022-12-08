16d2fdaf9d72f491d067af7eb5e06a8f258142bd
Jan Herber <janherber@gmx.de>
2022-12-08 16:02:23 +0100

    Add 2022 day6

---
+++ 2022/day6/day6_part1.py
@@ -0,0 +1,20 @@
+def findStartOfPacketMarker(lines, marker_length):
+    for element in range(marker_length - 1, len(lines)):
+        chars = []
+        for item in range(element - (marker_length - 1), element + 1):
+            if lines[item] not in chars:
+                chars.append(lines[item])
+            else:
+                break
+        if len(chars) == marker_length:
+            return element + 1
+
+
+def main():
+    with open("input.txt") as file:
+        lines = file.readline()
+        print(findStartOfPacketMarker(lines, 4))
+
+
+if __name__ == "__main__":
+    main()
